850642d911603c75438b6e0fca1588466b857592
hyprhex <swapapis@gmail.com>
2024-08-06 10:02:33 +0100

    Use JSON to responsed instead of a plain text

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -8,6 +9,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// Get ID from URL
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	// Retrive id from url
 	params := httprouter.ParamsFromContext(r.Context())
@@ -19,4 +21,35 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+// Write JSON response
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	// Encoding Object to JSON
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Newline for better view in Terminal
+	js = append(js, '\n')
+
+	// If header found add it to response
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// Application response type
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write status
+	w.WriteHeader(status)
+
+	// Write JSON to response body
+	_, err = w.Write(js)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
