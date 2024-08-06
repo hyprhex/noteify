ac27339eb73f19a1f6e0a3c4d4a8425cdd107fe4
hyprhex <swapapis@gmail.com>
2024-08-06 18:44:17 +0100

    Use MarshalIndent for view better on terminal

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -26,7 +26,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 // Write JSON response
 func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
 	// Encoding Object to JSON
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
